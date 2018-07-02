e0cc008852a0da82d5076a969e5972574860bb2c
Benjamin Sirb <bens@storj.io>
2018-07-02 14:44:55 -0400

    Update for python3, change to eps output

--- durability/durability.py
+++ durability/durability.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python
 
-import matplotlib as mpl
-mpl.use("pdf")
 import seaborn as sns
 from matplotlib import pylab as plt
-from decimal import Decimal as D, getcontext
 
-getcontext().prec = 100
 N = 40
 K = 32
 F = .1
 
 def binomial(n, k):
   if 0 <= k <= n:
-    ntok = D(1)
-    ktok = D(1)
-    for t in xrange(1, min(k, n - k) + 1):
+    ntok = 1
+    ktok = 1
+    for t in range(1, min(k, n - k) + 1):
       ntok *= n
-      ktok *= D(t)
-      n -= D(1)
+      ktok *= t
+      n -= 1
     return ntok // ktok
   else:
-    return D(0)
-
+    return 0
 
 def probdur(n, k, fail):
-  n, k, fail = D(n), D(k), D(fail)
-  sum = D(0)
-  for i in xrange(n-k+1):
-    i = D(i)
-    sum += binomial(n, i) * fail**i * (1-fail)**(n-i)
-  return sum
+  val = 0
+  for i in range(n-k+1):
+      val += binomial(n, i) * fail**i * (1-fail)**(n-i)
+  return val
 
 
 def nines(n, k, fail):
@@ -52,19 +45,21 @@ def nines(n, k, fail):
 colors = sns.cubehelix_palette(
     8, dark=0, light=1, start=.5, rot=-.75, as_cmap=True)
 ax = sns.heatmap(
-    [[nines(n, k, F) for n in xrange(1,N+1)]
-     for k in xrange(1,K+1)],
-    xticklabels=[i if i % 4 == 0 else "" for i in xrange(1, N+1)],
-    yticklabels=[i if i % 4 == 0 else "" for i in xrange(1, K+1)],
-    cmap=colors,
-    cbar_kws={"label": "9s of durability"})
+    [[nines(n, k, F) for n in range(1,N+1)]
+     for k in range(1,K+1)],
+         xticklabels=[i if i % 4 == 0 else "" for i in range(1, N+1)],
+         yticklabels=[i if i % 4 == 0 else "" for i in range(1, K+1)],
+         cmap=colors,
+         cbar_kws={"label": "9's of durability"},
+         vmin=0,
+         vmax=36)
 ax.invert_yaxis()
 plt.yticks(rotation=0)
-plt.title("durability assuming uniform %d%% probability of node failure" % (
-    int(F*100),))
+plt.title("Durability assuming uniform %d%% probability of node failure" % (
+    int(F*100)))
 plt.ylabel("k", rotation=0)
 plt.xlabel("n")
-plt.savefig("durability")
+plt.savefig("durability.eps")
 
 for k, n in ((2, 4), (4, 8), (8, 16), (16, 32), (32, 64)):
-  print k, n, nines(n, k, F), "%0.20f" % probdur(n, k, F)
+  print(k, n, nines(n, k, F), "%0.20f" % probdur(n, k, F))
